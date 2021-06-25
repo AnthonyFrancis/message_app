582e0376e12ec91ce517da80bf9cb10962114809
Anthony Francis <tonytrack@gmail.com>
2021-06-25 19:10:06 +0100

    add send me a message button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 	def show
 		@user = User.friendly.find(params[:id])
 		@conversation = current_user.conversations.build
-		@conversations = current_user.conversations.all.order("created_at ASC")
+		# @conversations = current_user.conversations.all.order("created_at ASC")
+		@conversations = Conversations.all.order("created_at ASC")
 	end
 end
